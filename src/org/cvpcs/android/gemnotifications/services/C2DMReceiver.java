09212d2a167ecad6467cb90e249ab0ed31c6d630
cvpcs <root@cvpcs.org>
2010-12-22 03:07:57 -0600

    make this actually partially usable by 2.0

--- src/org/cvpcs/android/gemnotifications/services/C2DMReceiver.java
+++ src/org/cvpcs/android/gemnotifications/services/C2DMReceiver.java
@@ -6,6 +6,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -44,14 +45,16 @@ public class C2DMReceiver extends C2DMBaseReceiver {
     @Override
     protected void onMessage(Context context, Intent intent) {
         Bundle extras = intent.getExtras();
-        String type = extras.getString("type");
+
+        // we assume we only have 1 type right now, and we just pass a url to the page we want to direct them to
+        String url = extras.getString("url");
 
         Log.d(TAG, "Got C2DM Message");
 
-        sendNotification(context, type);
+        sendNotification(context, url);
     }
 
-    private void sendNotification(Context context, String type) {
+    private void sendNotification(Context context, String url) {
         NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
         // Create Notification
@@ -59,11 +62,23 @@ public class C2DMReceiver extends C2DMBaseReceiver {
         long when = System.currentTimeMillis();
         Notification notification = new Notification(R.drawable.app_icon, tickerText, when);
         notification.defaults |= Notification.DEFAULT_SOUND;
+        notification.defaults |= Notification.FLAG_AUTO_CANCEL;
 
         // Define expanded view
         CharSequence contentTitle = getString(R.string.notif_update_title);
         CharSequence contentText = getString(R.string.notif_update_long);
-        Intent notificationIntent = new Intent(context, GEMNotifications.class);
+
+        // create our notification view
+        Intent notificationIntent;
+        try {
+            // try to open browser
+            notificationIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        } catch(Exception e) {
+            // FAIL
+            Log.e(TAG, "Error creating intent for browser", e);
+            notificationIntent = new Intent(context, GEMNotifications.class);
+        }
+
         PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
 
         notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
